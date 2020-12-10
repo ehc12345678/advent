57b64c722fd1eba6cbbdbb0f11af7135d0a6ea6d
Erik Currin <ecurrin@patientping.com>
2020-12-10 14:15:12 -0500

    Puzzle 10

--- src/main/kotlin/com/advent/puzzle10/Puzzle10.kt
+++ src/main/kotlin/com/advent/puzzle10/Puzzle10.kt
@@ -3,21 +3,24 @@ package com.advent.puzzle9
 import java.io.File
 
 data class Counts(
-    var count1: Int,
-    var count2: Int,
-    var count3: Int
+    var count1: Long,
+    var count2: Long,
+    var count3: Long
 )
 
 fun main() {
     val puzzle = Puzzle10()
     try {
         val nums = puzzle.readInputs("inputs.txt")
-        nums.add(nums.last() + 3)
+        val goal = nums.last() + 3
+        nums.add(goal)
         nums.forEach { println(it) }
-        val all = HashSet(nums)
         val counts = Counts(0, 0, 0)
-        puzzle.findPath(0, all, counts)
-        println("Value is ${counts.count1.toLong() * counts.count3.toLong()}")
+        puzzle.findPath(0, HashSet(nums), counts)
+        println("Value is ${counts.count1 * counts.count3}")
+
+        val allCount = puzzle.findAllPath2(nums, goal)
+        println("All count is $allCount")
 
     } catch (e: Throwable) {
         e.printStackTrace()
@@ -52,6 +55,22 @@ class Puzzle10 {
             found = findPath(plusThree, all, counts)
         }
         return found
+    }
+
+    fun findAllPath2(nums: List<Int>, goal: Int): Long {
+        val map = HashMap<Int, Long>()
+        map[goal] = 1L
+        for (index in nums.size - 2 downTo 0) {
+            val num = nums[index]
+            map[num] =
+                map.getOrDefault(num + 1, 0) +
+                map.getOrDefault(num + 2, 0) +
+                map.getOrDefault(num + 3, 0)
+        }
+        val num = 0
+        return map.getOrDefault(num + 1, 0) +
+            map.getOrDefault(num + 2, 0) +
+            map.getOrDefault(num + 3, 0)
     }
 
 }
