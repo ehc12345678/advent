6dc17e64e2d599b378b1d8e353ad70bd1f693e8e
Erik Currin <ecurrin@patientping.com>
2020-12-15 10:38:34 -0500

    Puzzle 15

---
+++ src/main/kotlin/com/advent/puzzle15/Puzzle15.kt
@@ -0,0 +1,37 @@
+package com.advent.puzzle15
+
+fun main() {
+    val puzzle = Puzzle15()
+    try {
+        val answer : Int = puzzle.puzzleFindNth(listOf(2, 1, 10, 11, 0, 6), 2020)
+        println("For 2020, answer is $answer")
+
+        val answer2 = puzzle.puzzleFindNth(listOf(2, 1, 10, 11, 0, 6), 30000000)
+        println("For 2020, answer is $answer2")
+    } catch (e: Throwable) {
+        e.printStackTrace()
+    }
+}
+
+typealias NumberMap = HashMap<Int, Int>
+class Puzzle15 {
+    fun puzzleFindNth(inputs: List<Int>, n: Int): Int {
+        var i = 1
+        val map = NumberMap() // number to index
+        var next = 0
+        inputs.forEach {
+            next = say(i++, it, map)
+        }
+        for (x in i..n) {
+            next = say(x, next, map)
+        }
+        return map.filter { it.value == n }.iterator().next().key
+    }
+
+    fun say(index: Int, number: Int, map: NumberMap) : Int {
+        val gap = index - map.getOrDefault(number, index)
+        map[number] = index
+        return gap
+    }
+
+}
