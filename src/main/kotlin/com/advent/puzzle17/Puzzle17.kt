17854c17651ca52943aa5e3da6c8443ee45c0000
Erik Currin <ecurrin@patientping.com>
2020-12-17 09:54:53 -0500

    Duplicate code

--- src/main/kotlin/com/advent/puzzle17/Puzzle17.kt
+++ src/main/kotlin/com/advent/puzzle17/Puzzle17.kt
@@ -3,19 +3,13 @@ package com.advent.puzzle17
 import java.io.File
 
 data class Point(val x: Int, val y: Int, val z: Int)
-data class PointND(val dims: Int) {
-    val coords = Array(dims) { 0 }
-}
-infix fun Point.add(other: Point) : Point = Point(x + other.x, y + other.y, z + other.z)
-infix fun PointND.add(other: PointND) : PointND {
-    val ret = PointND(other.dims)
-    for (index in coords.indices) {
-        ret.coords[index] = coords[index] + other.coords[index]
-    }
-    return ret
-}
+data class Point4D(val x: Int, val y: Int, val z: Int, val w: Int)
+
+infix fun Point.add(other: Point)  = Point(x + other.x, y + other.y, z + other.z)
+infix fun Point4D.add(other: Point4D)  = Point4D(x + other.x, y + other.y, z + other.z, w + other.z)
+
 typealias Cubes = HashSet<Point>
-typealias CubesND = HashSet<Point>
+typealias Cubes4D = HashSet<Point4D>
 
 fun main() {
     val puzzle = Puzzle17()
@@ -23,6 +17,10 @@ fun main() {
         val data = puzzle.readInputs("inputs.txt")
         val answer = puzzle.partA(data)
         println("Answer is $answer")
+
+        val dataB = puzzle.readInputsB("inputs.txt")
+        val answer2 = puzzle.partB(dataB)
+        println("Answer is $answer2")
     } catch (e: Throwable) {
         e.printStackTrace()
     }
@@ -30,8 +28,10 @@ fun main() {
 
 class Puzzle17 {
     private val neighbors: Set<Point>
+    private val neighbors4D: Set<Point4D>
     init {
         neighbors = calcNeighbors()
+        neighbors4D = calcNeighbors4D()
     }
 
     fun readInputs(filename: String): Cubes {
@@ -48,7 +48,21 @@ class Puzzle17 {
         return ret
     }
 
-    fun partA(data: java.util.HashSet<Point>): Int {
+    fun readInputsB(filename: String): Cubes4D {
+        val file = File(filename)
+        val lines = file.readLines()
+        val ret = Cubes4D()
+        lines.forEachIndexed { yIndex, line ->
+            line.forEachIndexed { xIndex, ch ->
+                if (ch == '#') {
+                    ret.add(Point4D(xIndex, yIndex, 0, 0))
+                }
+            }
+        }
+        return ret
+    }
+
+    fun partA(data: Cubes): Int {
         var newState = data
         for (i in 0 until 6) {
             newState = doOneTurn(newState)
@@ -96,5 +110,57 @@ class Puzzle17 {
 
     private fun countNeighbors(point: Point, data: Cubes): Int {
         return getAllNeighbors(point).fold(0) { acc, it -> acc + if (data.contains(it)) 1 else 0 }
+    }
+
+    fun partB(data: Cubes4D): Int {
+        var newState = data
+        for (i in 0 until 6) {
+            newState = doOneTurn4D(newState)
+        }
+        return newState.size
+    }
+
+    private fun calcNeighbors4D() : Set<Point4D> {
+        val ret = HashSet<Point4D>()
+        for (x in -1..1) {
+            for (y in -1..1) {
+                for (z in -1..1) {
+                    for (w in -1..1) {
+                        if (!(x == 0 && y == 0 && z == 0 && w == 0)) {
+                            ret.add(Point4D(x, y, z, w))
+                        }
+                    }
+                }
+            }
+        }
+        return ret
+    }
+
+    private fun doOneTurn4D(data: Cubes4D) : Cubes4D {
+        val ret = Cubes4D()
+        data.forEach {
+            // active
+            if (countNeighbors(it, data) in 2..3) {
+                ret.add(it)
+            }
+
+            // inactive
+            val allMyNeighbors = getAllNeighbors4D(it)
+            allMyNeighbors.forEach { neighbor ->
+                if (!data.contains(neighbor) && countNeighbors(neighbor, data) == 3) {
+                    ret.add(neighbor)
+                }
+            }
+        }
+
+        return ret
+    }
+
+    private fun getAllNeighbors4D(point: Point4D): List<Point4D> {
+        return neighbors4D.map { it add point }
+    }
+
+    private fun countNeighbors(point: Point4D, data: Cubes4D): Int {
+        return getAllNeighbors4D(point).fold(0) { acc, it -> acc + if (data.contains(it)) 1 else 0 }
     }
 }
