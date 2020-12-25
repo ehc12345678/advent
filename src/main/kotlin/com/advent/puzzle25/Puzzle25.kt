a233ec5c0f51299996c57b7972d8109ebca28a30
Erik Currin <ecurrin@patientping.com>
2020-12-25 10:20:00 -0500

    Day 25 complete

--- src/main/kotlin/com/advent/puzzle25/Puzzle25.kt
+++ src/main/kotlin/com/advent/puzzle25/Puzzle25.kt
@@ -1,9 +1,11 @@
 package com.advent.puzzle25
 
+import com.advent.puzzle13.Bus
 import java.io.File
+import java.lang.RuntimeException
+import java.math.BigInteger
 
-typealias Item = String
-typealias Data = List<Item>
+typealias Data = Pair<Long, Long>
 
 fun main() {
     val puzzle = Puzzle25()
@@ -11,9 +13,6 @@ fun main() {
         val data = puzzle.readInputs("inputs.txt")
         val answerA = puzzle.solutionA(data)
         println("Answer A is $answerA")
-
-        val answerB = puzzle.solutionB(data)
-        println("Answer B is $answerB")
     } catch (e: Throwable) {
         e.printStackTrace()
     }
@@ -23,19 +22,42 @@ class Puzzle25 {
     fun readInputs(filename: String): Data {
         val file = File(filename)
         val lines = file.readLines()
-        val data = lines.map { parseLine(it) }
-        return data
-    }
-
-    fun parseLine(line: String) : Item {
-        return line
+        return Data(lines[0].toLong(), lines[1].toLong())
     }
 
     fun solutionA(data: Data) : Long {
-        return 0
+        val cardPublicKey = data.first
+        val doorPublicKey = data.second
+
+        val mod: Long = 20201227
+        val cardLoopSize = transformSubjectNumber(7, cardPublicKey, mod)
+        val doorLoopSize = transformSubjectNumber(7, doorPublicKey, mod)
+
+        val doorEncrypt = invert(doorPublicKey, cardLoopSize, mod)
+        val cardEncrypt = invert(cardPublicKey, doorLoopSize, mod)
+        if (doorEncrypt != cardEncrypt) {
+            throw RuntimeException("Oops")
+        }
+        return doorEncrypt
     }
 
-    fun solutionB(data: Data) : Long {
-        return 0
+    fun transformSubjectNumber(subject: Long, target: Long, mod: Long) : Long {
+        var now = 1L
+        var step = 0L
+        do {
+            now = (now * subject) % mod
+            ++step
+        } while (now != target)
+
+        return step
     }
+
+    fun invert(subject: Long, loopSize: Long, mod: Long) : Long {
+        var now = 1L
+        for (i in 0 until loopSize) {
+            now = (now * subject) % mod
+        }
+        return now
+    }
+
 }
