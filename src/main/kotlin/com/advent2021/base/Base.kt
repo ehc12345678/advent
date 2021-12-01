05ad0c9e6ab2510e2b6544fcda17e9809f37fb04
Erik Currin <ehc12345678@gmail.com>
2021-12-01 07:50:17 -0500

    Day 1: 2021

--- src/main/kotlin/com/advent2021/base/Base.kt
+++ src/main/kotlin/com/advent2021/base/Base.kt
@@ -1,28 +1,37 @@
 package com.advent2021.base
 
 import java.io.File
+import java.io.IOException
 
 abstract class Base<T, V, U> {
+    @Throws(IOException::class)
     open fun readInput(filename: String, data: T): T {
-        val file = File(filename)
-        file.readLines().forEach {
-            parseLine(it, data)
+        try {
+            val file = File(filename)
+            file.readLines().forEach {
+                parseLine(it, data)
+            }
+            return data
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            throw ex
         }
-        return data
     }
 
-    abstract open fun parseLine(line: String, data: T)
+    abstract fun parseLine(line: String, data: T)
 
+    @Throws(IOException::class)
     open fun solvePuzzle(filename: String, data: T) : V {
         val newData = readInput(filename, data)
         return computeSolution(data)
     }
 
+    @Throws(IOException::class)
     open fun solvePuzzle2(filename: String, data: T) : U {
         val newData = readInput(filename, data)
         return computeSolution2(data)
     }
 
-    abstract open fun computeSolution(data: T): V
-    abstract open fun computeSolution2(data: T): U
+    abstract fun computeSolution(data: T): V
+    abstract fun computeSolution2(data: T): U
 }
--- src/main/kotlin/com/advent2021/puzzle1/Puzzle1.kt
+++ src/main/kotlin/com/advent2021/puzzle1/Puzzle1.kt
@@ -2,37 +2,57 @@ package com.advent.advent2021.puzzle1
 
 import com.advent2021.base.Base
 
-typealias Data = HashSet<Int>
-typealias Solution = Pair<Int, Int>
-typealias Solution2 = HashSet<Int>
+typealias Data = ArrayList<Int>
+data class Solution(
+   var count: Int
+)
+typealias Solution2 = Solution
 
-val sum = 2020
 fun main() {
-    val puz = Puzzle1()
-    val solution1 = puz.solvePuzzle("inputs.txt", Data())
-    if (solution1 != null) {
-        // todo
-    }
-    val solution2 = puz.solvePuzzle2("inputs.txt", Data())
-    if (solution2 != null) {
-        // todo
+    try {
+        val puz = Puzzle1()
+        val solution1 = puz.solvePuzzle("inputs.txt", Data())
+        println("Solution1: $solution1")
+
+        val solution2 = puz.solvePuzzle2("inputs.txt", Data())
+        println("Solution2: $solution2")
+    } catch (t: Throwable) {
+        t.printStackTrace()
     }
 }
 
 class Puzzle1 : Base<Data, Solution?, Solution2?>() {
     override fun parseLine(line: String, data: Data) {
-        // data.add(line.toInt())
-        // todo
+        data.add(line.toInt())
     }
 
-    override fun computeSolution(data: Data): Solution? {
-        // todo
-        return null
+    override fun computeSolution(data: Data): Solution {
+        var last = data[0]
+        val solution = Solution(0)
+        data.subList(1, data.size).forEach {
+            if (it > last) {
+                solution.count++
+            }
+            last = it
+        }
+        return solution
     }
 
-    override fun computeSolution2(data: Data): Solution2? {
-        // todo
-        return null
+    override fun computeSolution2(data: Data): Solution2 {
+        var last = sumWindow(data, 0)
+        val solution = Solution2(0)
+        (1..data.size - 3).forEach { index ->
+            val window = sumWindow(data, index)
+            if (window > last) {
+                solution.count++
+            }
+            last = window
+        }
+        return solution
+    }
+
+    private fun sumWindow(data: Data, index: Int) : Int {
+        return data.subList(index, index + 3).reduce { acc, i -> acc + i }
     }
 }
 
