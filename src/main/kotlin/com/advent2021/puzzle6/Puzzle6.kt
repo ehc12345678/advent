2699f5f4c48197ef3d5c431bfc897fc06b4bcf6c
Erik Currin <ehc12345678@gmail.com>
2021-12-06 09:02:28 -0500

    Day 6

--- src/main/kotlin/com/advent2021/puzzle6/Puzzle6.kt
+++ src/main/kotlin/com/advent2021/puzzle6/Puzzle6.kt
@@ -1,12 +1,11 @@
 package com.advent2021.puzzle6
 
 import com.advent2021.base.Base
+import java.math.BigInteger
 
 typealias Data = ArrayList<Int>
-data class Solution(
-    var count: Int
-)
-typealias Solution2 = Solution
+typealias Solution = BigInteger
+typealias Solution2 = BigInteger
 
 fun main() {
     try {
@@ -23,14 +22,28 @@ fun main() {
 
 class Puzzle6 : Base<Data, Solution?, Solution2?>() {
     override fun parseLine(line: String, data: Data) {
-        data.add(line.toInt())
+        data.addAll(line.split(",").map { it.toInt() })
     }
 
-    override fun computeSolution(data: Data): Solution {
-        return Solution(0)
+    override fun computeSolution(data: Data): Solution = solution(data, 80)
+    override fun computeSolution2(data: Data): Solution2 = solution(data, 256)
+
+    private fun solution(data: Data, days: Int): BigInteger {
+        val cnts = Array(9) { 0.toBigInteger() }
+        data.forEach { cnts[it]++ }
+        for (i in 0 until days) {
+            step1(cnts)
+        }
+        return cnts.sumOf { it }
     }
-    override fun computeSolution2(data: Data): Solution2 {
-        return Solution(0)
+
+    private fun step1(sol: Array<BigInteger>) {
+        val zero = sol[0]
+        for (i in 1 until sol.size) {
+            sol[i - 1] = sol[i]
+        }
+        sol[sol.size - 1] = zero
+        sol[6] += zero
     }
 }
 
