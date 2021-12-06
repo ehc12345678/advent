1e5941c7830577413aa06d2af527c2ea1e328396
Erik Currin <ehc12345678@gmail.com>
2021-12-05 19:42:19 -0500

    Refactor

--- src/main/kotlin/com/advent2021/puzzle5/Puzzle5.kt
+++ src/main/kotlin/com/advent2021/puzzle5/Puzzle5.kt
@@ -2,8 +2,12 @@ package com.advent2021.puzzle5
 
 import com.advent2021.base.Base
 
-data class Point(val x: Int, val y: Int)
-data class Line(val pt1: Point, val pt2: Point)
+data class Point(val x: Int, val y: Int) {
+    constructor(array: List<Int>) : this(array[0], array[1])
+}
+data class Line(val pt1: Point, val pt2: Point) {
+    constructor(pts: List<Point>) : this(pts[0], pts[1])
+}
 class Data(
     val lines: ArrayList<Line> = ArrayList(),
 )
@@ -20,23 +24,18 @@ class Solution {
         var y = pt1.y
         addPoint(x, y)
 
-        val x2 = pt2.x
-        val y2 = pt2.y
-        val stepX = step(x, x2)
-        val stepY = step(y, y2)
+        val stepX = step(x, pt2.x)
+        val stepY = step(y, pt2.y)
         do {
             x += stepX
             y += stepY
             addPoint(x, y)
-        } while (x != x2 || y != y2)
+        } while (x != pt2.x || y != pt2.y)
     }
 
     private fun step(x: Int, x2: Int) = if (x > x2) -1 else if (x < x2) 1 else 0
 
-    fun addPoint(x: Int, y: Int) {
-        val point = Point(x, y)
-        pointMap[point] = pointMap.getOrDefault(point, 0) + 1
-    }
+    fun addPoint(x: Int, y: Int) = Point(x, y).also { pt -> pointMap[pt] = pointMap.getOrDefault(pt, 0) + 1 }
 
     fun solution() = pointMap.entries.count { it.value >= 2 }
 }
@@ -44,7 +43,7 @@ typealias Solution2 = Solution
 
 fun main() {
     try {
-        val puz = Puzzle1()
+        val puz = Puzzle5()
         val solution1 = puz.solvePuzzle("inputs.txt", Data())
         println("Solution1: ${solution1?.solution()}")
 
@@ -55,27 +54,24 @@ fun main() {
     }
 }
 
-class Puzzle1 : Base<Data, Solution?, Solution2?>() {
+fun String.toPoint() = Point(split(",").map { it.toInt() })
+fun String.toLine() = Line(split(" -> ").map { it.toPoint() })
+
+class Puzzle5 : Base<Data, Solution?, Solution2?>() {
     override fun parseLine(line: String, data: Data) {
-        val points = line.split(" -> ")
-        val pt1 = points[0].split(",")
-        val pt2 = points[1].split(",")
-        data.lines.add(Line(Point(pt1[0].toInt(), pt1[1].toInt()), Point(pt2[0].toInt(), pt2[1].toInt())))
+        data.lines.add(line.toLine())
     }
 
-    override fun computeSolution(data: Data): Solution {
-        val sol = Solution()
-        data.lines.forEach {
-            sol.addPoints(it, false)
+    override fun computeSolution(data: Data): Solution = computeSolution(data, false)
+    override fun computeSolution2(data: Data): Solution = computeSolution(data, true)
+
+    private fun computeSolution(data: Data, diagonals: Boolean): Solution {
+        return Solution().also { sol ->
+            data.lines.forEach {
+                sol.addPoints(it, diagonals)
+            }
         }
-        return sol
     }
-    override fun computeSolution2(data: Data): Solution2 {
-        val sol = Solution()
-        data.lines.forEach {
-            sol.addPoints(it, true)
-        }
-        return sol
-    }
+
 }
 
