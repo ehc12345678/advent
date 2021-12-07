300ac4c4faaf2974a458b071ff84769433a98260
Erik Currin <ehc12345678@gmail.com>
2021-12-07 09:27:48 -0500

    Preflight 8

---
+++ src/main/kotlin/com/advent2021/puzzle8/Puzzle8.kt
@@ -0,0 +1,34 @@
+package com.advent2021.puzzle8
+
+import com.advent2021.base.Base
+
+typealias Data = ArrayList<Int>
+typealias Solution = Int
+typealias Solution2 = Solution
+
+fun main() {
+    try {
+        val puz = Puzzle8()
+        val solution1 = puz.solvePuzzle("inputs.txt", Data())
+        println("Solution1: $solution1")
+
+        val solution2 = puz.solvePuzzle2("inputs.txt", Data())
+        println("Solution2: $solution2")
+    } catch (t: Throwable) {
+        t.printStackTrace()
+    }
+}
+
+class Puzzle8 : Base<Data, Solution?, Solution2?>() {
+    override fun parseLine(line: String, data: Data) {
+        data.add(line.toInt())
+    }
+
+    override fun computeSolution(data: Data): Solution {
+        return 0
+    }
+    override fun computeSolution2(data: Data): Solution2 {
+        return 0
+    }
+}
+
