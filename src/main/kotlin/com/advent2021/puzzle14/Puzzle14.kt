78df09ec39a8f0e253612a773a6c421581c7b6b4
Erik Currin <ehc12345678@gmail.com>
2021-12-14 19:32:26 -0500

    Puzzle 14

--- src/main/kotlin/com/advent2021/puzzle14/Puzzle14.kt
+++ src/main/kotlin/com/advent2021/puzzle14/Puzzle14.kt
@@ -21,7 +21,7 @@ fun main() {
         val solution1 = puz.solvePuzzle("inputs.txt", Data())
         println("Solution1: $solution1")
 
-        val solution2 = puz.solvePuzzle2("inputsTest.txt", Data())
+        val solution2 = puz.solvePuzzle2("inputs.txt", Data())
         println("Solution2: $solution2")
     } catch (t: Throwable) {
         t.printStackTrace()
@@ -61,17 +61,10 @@ class Puzzle14 : Base<Data, Solution?, Solution2?>() {
         return newString.toString()
     }
 
-//    private fun doStepWithMath(inLength: Long): Long {
-//        return ((inLength - 1) * 2) + 1
-//    }
+    private fun calcMaxMinusMin(str: String): Long = calcMaxMinusMin(getCharCounts(str))
 
-    private fun calcMaxMinusMin(str: String): Long {
-        val counts = getCharCounts(str)
-
-        val minEntry = counts.entries.minOf { it.value }
-        val maxEntry = counts.entries.maxOf { it.value }
-        return maxEntry - minEntry
-    }
+    private fun calcMaxMinusMin(counts: CharCount): Long =
+        counts.entries.maxOf { it.value } - counts.entries.minOf { it.value }
 
     private fun getCharCounts(str: String): CharCount {
         val counts = HashMap<Char, Long>()
@@ -83,17 +76,15 @@ class Puzzle14 : Base<Data, Solution?, Solution2?>() {
 
     override fun computeSolution2(data: Data): Solution2 {
         val working = Working()
-        for (key in data.transforms.keys) {
-            getCounts(key, working, 3, data)
-        }
+        val start = data.start!!
 
-//        var working = data.start!!.substring(0,2)
-//        for (step in 0 until 40) {
-//            println("step $step")
-//            working = doStep(working, data)
-//        }
-//        return calcMaxMinusMin(working)
-        return 0
+        val numSteps = 40
+        var charCounts = getCounts(pairToStr(start[0], start[1]), working, numSteps, data)
+        for (i in 1 until start.length - 1) {
+            val newCounts = getCounts(pairToStr(start[i], start[i + 1]), working, numSteps, data)
+            charCounts = combineCounts(charCounts, newCounts, start[i])
+        }
+        return calcMaxMinusMin(charCounts)
     }
 
     private fun getCounts(pair: String, working: Working, numSteps: Int, data: Data): CharCount {
@@ -104,20 +95,26 @@ class Puzzle14 : Base<Data, Solution?, Solution2?>() {
                 charCount = getCharCounts(key.pair)
             } else {
                 val ch = data.transforms[pair] ?: throw IllegalArgumentException("Couldn't find $pair")
-                charCount = getCounts(pairToStr(pair[0], ch), working, numSteps - 1, data)
+                val count1 = getCounts(pairToStr(pair[0], ch), working, numSteps - 1, data)
                 val count2 = getCounts(pairToStr(ch, pair[1]), working, numSteps - 1, data)
-                for (count2Ch in count2) {
-                    val newCh = count2Ch.key
-                    var newCount = charCount.getOrDefault(newCh, 0) + count2Ch.value
-                    if (newCh == ch) {
-                        newCount -= 1
-                    }
-                    charCount[newCh] = newCount
-                }
+                charCount = combineCounts(count1, count2, ch)
             }
             working[key] = charCount
         }
         return charCount
+    }
+
+    private fun combineCounts(count1: CharCount, count2: CharCount, ch: Char): CharCount {
+        val ret = CharCount(count1)
+        for (count2Ch in count2) {
+            val newCh = count2Ch.key
+            var newCount = ret.getOrDefault(newCh, 0) + count2Ch.value
+            if (newCh == ch) {
+                newCount -= 1
+            }
+            ret[newCh] = newCount
+        }
+        return ret
     }
 
     private fun pairToStr(ch1: Char, ch2: Char) = ch1.toString() + ch2
