31fec5be39f12ce1ae2a77f646c81081aa6b1aab
Erik Currin <ehc12345678@gmail.com>
2021-12-15 19:54:00 -0500

    Puzzle 15, part A

--- src/main/kotlin/com/advent2021/puzzle15/Puzzle15.kt
+++ src/main/kotlin/com/advent2021/puzzle15/Puzzle15.kt
@@ -1,17 +1,17 @@
 package com.advent2021.puzzle15
 
 import com.advent2021.base.Base
-import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 import kotlin.collections.LinkedHashSet
 
 data class Point(val row: Int, val col: Int)
-class Square(val point: Point, var num: Int)
+data class Square(val point: Point, var num: Int)
 
 typealias Line = ArrayList<Square>
 class Data {
     val grid: ArrayList<Line> = ArrayList()
+    fun value(pt: Point) = value(pt.row, pt.col)
     fun value(r: Int, c: Int): Square? = if (r in grid.indices && c in grid[r].indices) grid[r][c] else null
     fun neighbors(pt: Point): Set<Square> {
         val r = pt.row
@@ -27,11 +27,14 @@ class Data {
     val endPoint: Point
         get() = Point(rows() - 1, cols() - 1)
 }
-data class Path(val squares: LinkedHashSet<Square>) {
+data class Path(
+    val squares: LinkedHashSet<Square>,
+    var isDefinitelyShortest: Boolean = false
+) {
     fun score() = if (squares.isEmpty()) {
         Integer.MAX_VALUE
     } else {
-        squares.sumOf { it.num } - squares.first().num
+        squares.sumOf { it.num }
     }
 
     val endOfPath: Square
@@ -50,7 +53,7 @@ typealias Working = HashMap<Square, Path>
 fun main() {
     try {
         val puz = Puzzle15()
-        val solution1 = puz.solvePuzzle("inputsTest.txt", Data())
+        val solution1 = puz.solvePuzzle("inputs.txt", Data())
         println("Solution1: $solution1")
 
         val solution2 = puz.solvePuzzle2("inputs.txt", Data())
@@ -76,35 +79,71 @@ class Puzzle15 : Base<Data, Solution?, Solution2?>() {
     }
 
     override fun computeSolution(data: Data): Solution {
-        val firstPath = LinkedHashSet<Square>().also { it.add(data.value(0,0)!!) }
-        val working = Working()
-        val bestPath = dftSol(Path(firstPath), data, working)
-        return bestPath.score()
-    }
+        val graph = Graph<Square>()
 
-    private fun dftSol(currentPath: Path, data: Data, working: Working): Path {
-        val square = currentPath.endOfPath
-        if (working.containsKey(square)) {
-            return working[square]!!
-        }
-        if (square.point == data.endPoint) {
-            return currentPath
-        }
+        // build a dijkstra weighted graph
+        for (line in data.grid) {
+            for (sq in line) {
+                graph.vertices.add(sq)
 
-        var bestPath = NO_PATH
-        val neighbors = data.neighbors(square.point) - currentPath.squares
-        for (neighbor in neighbors) {
-            val newPath = Path(LinkedHashSet(currentPath.squares).also { it.add(neighbor) })
-            val bestNewPath = dftSol(newPath, data, working)
-            val bestPathScore = bestPath.score()
-            val bestNewPathScore = bestNewPath.score()
-            if (bestNewPathScore < bestPathScore) {
-                println("${square.point} Found new bestPath ${bestNewPath} with score $bestNewPathScore")
-                bestPath = bestNewPath
+                val neighbors = data.neighbors(sq.point)
+                graph.edges[sq] = HashSet(neighbors.filter { !graph.vertices.contains(it) })
+                for (neighbor in neighbors) {
+                    if (!graph.vertices.contains(neighbor)) {
+                        graph.weights[Pair(sq, neighbor)] = neighbor.num
+                    }
+                }
             }
         }
-        working[square] = bestPath
-        return bestPath
+
+        val start = data.value(0, 0)!!
+        val end = data.value(data.endPoint)!!
+        val shortPathTree = dijkstra(graph, start)
+        val path = shortestPath(shortPathTree, start, end)
+        return path.sumOf { it.num } - path.first().num
+    }
+
+    class Graph<T> {
+        val vertices: HashSet<T> = HashSet()
+        val edges: HashMap<T, HashSet<T>> = HashMap()
+        val weights: HashMap<Pair<T, T>, Int> = HashMap()
+    }
+
+    fun <T> dijkstra(graph: Graph<T>, start: T): Map<T, T?> {
+        val S: MutableSet<T> = mutableSetOf() // a subset of vertices, for which we know the true distance
+
+        val delta = graph.vertices.map { it to Int.MAX_VALUE }.toMap().toMutableMap()
+        delta[start] = 0
+
+        val previous: MutableMap<T, T?> = graph.vertices.map { it to null }.toMap().toMutableMap()
+
+        while (S != graph.vertices) {
+            val v: T = delta
+                .filter { !S.contains(it.key) }
+                .minByOrNull { it.value }!!
+                .key
+
+            graph.edges.getValue(v).minus(S).forEach { neighbor ->
+                val newPath = delta.getValue(v) + graph.weights.getValue(Pair(v, neighbor))
+
+                if (newPath < delta.getValue(neighbor)) {
+                    delta[neighbor] = newPath
+                    previous[neighbor] = v
+                }
+            }
+
+            S.add(v)
+        }
+
+        return previous.toMap()
+    }
+
+    fun <T> shortestPath(shortestPathTree: Map<T, T?>, start: T, end: T): List<T> {
+        fun pathTo(start: T, end: T): List<T> {
+            if (shortestPathTree[end] == null) return listOf(end)
+            return listOf(pathTo(start, shortestPathTree[end]!!), listOf(end)).flatten()
+        }
+        return pathTo(start, end)
     }
 
     override fun computeSolution2(data: Data): Solution2 {
