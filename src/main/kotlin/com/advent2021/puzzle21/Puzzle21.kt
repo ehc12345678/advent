264df4d9f96bd309d86d1d19b168ca587428b7dc
Erik Currin <ehc12345678@gmail.com>
2021-12-21 20:33:48 -0500

    Day 21 checkpoint

--- src/main/kotlin/com/advent2021/puzzle21/Puzzle21.kt
+++ src/main/kotlin/com/advent2021/puzzle21/Puzzle21.kt
@@ -1,6 +1,8 @@
 package com.advent2021.puzzle21
 
 import com.advent2021.base.Base
+import java.lang.IllegalStateException
+import java.math.BigInteger
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -11,10 +13,9 @@ open class Dice(var number: Int) {
     }
     fun wrap(number: Int, mod: Int, base: Int) = ((number - base) % mod) + base
     open fun roll(): Int = 0
-    fun copy() = Dice(number)
 }
 data class Player(val dice: Dice, var score: Int = 0) {
-    fun copy() = Player(dice.copy(), score)
+    fun toState() = PlayerState(dice.number, score)
 }
 
 class DeterministicDice : Dice(1) {
@@ -22,9 +23,23 @@ class DeterministicDice : Dice(1) {
 }
 typealias Data = ArrayList<Player>
 typealias Solution = Int
-typealias Solution2 = Solution
+typealias Solution2 = BigInteger
 
-fun Data.copy(): Data = Data(this)
+data class PlayerState(
+    val diceNumber: Int,
+    val score: Int
+)
+data class GameState(
+    val player1: PlayerState,
+    val player2: PlayerState,
+    val player1Turn: Boolean
+)
+
+data class WinsCount(
+    val player1Wins: BigInteger,
+    val player2Wins: BigInteger
+)
+typealias Working = HashMap<GameState, WinsCount>
 
 fun main() {
     try {
@@ -32,7 +47,7 @@ fun main() {
         val solution1 = puz.solvePuzzle("inputs.txt", Data())
         println("Solution1: $solution1")
 
-        val solution2 = puz.solvePuzzle2("inputs.txt", Data())
+        val solution2 = puz.solvePuzzle2("inputsTest.txt", Data())
         println("Solution2: $solution2")
     } catch (t: Throwable) {
         t.printStackTrace()
@@ -68,43 +83,80 @@ class Puzzle21 : Base<Data, Solution?, Solution2?>() {
     }
 
     override fun computeSolution2(data: Data): Solution2 {
-        val playerOneTurnGames = Stack<Data>()
-        playerOneTurnGames.add(data)
+        val working = Working()
 
-        val playerTwoTurnGames = Stack<Data()
-
-        for (turn in 0 until 3) {
-            while (playerOneTurnGames.isNotEmpty()) {
-                val top = playerOneTurnGames.pop()
-                val newList = doTurn2(top, top[0])
-                playerTwoTurnGames.addAll(newList)
+        var playerOneScore = 20
+        while (playerOneScore >= data[0].dice.number) {
+            var playerTwoScore = 20
+            while (playerTwoScore >= data[1].dice.number) {
+                computeWins(playerOneScore, playerTwoScore, true, working)
+                computeWins(playerOneScore, playerTwoScore, false, working)
+                --playerTwoScore
             }
-            while (playerTwoTurnGames.isNotEmpty()) {
-                val top = playerTwoTurnGames.pop()
-                val newList = doTurn2(top, top[1])
-                playerOneTurnGames.addAll(newList)
-            }
+            --playerOneScore
         }
-        val diceRolls = 0
-        return diceRolls
+
+        val answer = working[GameState(data[0].toState(), data[1].toState(), true)]!!
+        return if (answer.player1Wins > answer.player2Wins) answer.player1Wins else answer.player2Wins
     }
 
-    fun doTurn2(game: Data, player: Player): List<Data> {
-        val produced = ArrayList<Data>()
-        for (dice in 1..3) {
-            val firstPlayer: Player = game[0].copy().also {
-                if (player == it) {
-                    it.dice.add(dice)
+    fun computeWins(playerOneScore: Int, playerTwoScore: Int, playerOneTurn: Boolean, working: Working) {
+        for (dice in 1..10) {
+            val playersDice = Dice(dice)
+            var numPlayer1Wins = BigInteger.valueOf(0)
+            var numPlayer2Wins = BigInteger.valueOf(0)
+
+            val player1 = PlayerState(dice, playerOneScore)
+            val player2 = PlayerState(dice, playerTwoScore)
+            val player = if (playerOneTurn) player1 else player2
+            val otherPlayer = if (playerOneTurn) player2 else player1
+            val thisState = GameState(player1, player2, playerOneTurn)
+            for (newDice in getDiceInDescendingOrder(playersDice)) {
+                val thisScore = newDice.number + player.score
+                if (thisScore >= 21) {
+                    if (playerOneTurn) {
+                        ++numPlayer1Wins
+                    } else {
+                        ++numPlayer2Wins
+                    }
+                } else {
+                    for (otherDice in getDiceInDescendingOrder(playersDice)) {
+                        val otherScore = otherDice.number + otherPlayer.score
+                        if (otherScore >= 21) {
+                            if (playerOneTurn) {
+                                ++numPlayer2Wins
+                            } else {
+                                ++numPlayer1Wins
+                            }
+                        }
+                        else {
+                            val otherState =
+                                GameState(
+                                    PlayerState(newDice.number, thisScore),
+                                    PlayerState(otherDice.number, otherScore),
+                                    !playerOneTurn
+                                )
+                            val lookup = working[otherState]
+                            if (lookup == null) {
+                                throw IllegalStateException()
+                            }
+                            numPlayer1Wins += lookup.player1Wins
+                            numPlayer2Wins += lookup.player2Wins
+                        }
+                    }
                 }
             }
-            val secondPlayer: Player = game[1].copy().also {
-                if (player == it) {
-                    it.dice.add(dice)
-                }
-            }
-            produced.add(Data(listOf(firstPlayer, secondPlayer)))
+            working[thisState] = WinsCount(numPlayer1Wins, numPlayer2Wins)
         }
-        return produced
+    }
+
+    private fun getDiceInDescendingOrder(baseDice: Dice): List<Dice> {
+        val theseDice = ArrayList<Dice>()
+        for (quantum in 1..3) {
+            theseDice.add(Dice(quantum).also { it.add(baseDice.number) })
+        }
+        theseDice.sortByDescending { it.number }
+        return theseDice
     }
 
 }
