3b539ea033148f3cf29b188dfaf1ffd52c940c5c
Erik Currin <ehc12345678@gmail.com>
2022-12-17 15:59:09 -0500

    Part 1

--- src/main/kotlin/com/advent2022/puzzle16/Puzzle16.kt
+++ src/main/kotlin/com/advent2022/puzzle16/Puzzle16.kt
@@ -1,12 +1,10 @@
 package com.advent2022.puzzle16
 
 import com.advent2021.base.Base
+import kotlin.math.max
 
 class Valve(val name: String, val flowRate: Int, tunnels: List<String>) {
     val children = ArrayList<String>(tunnels.map { it.trim() })
-
-    // the best flow rate we can get on this node given the number of steps left
-    val bestFlowRate = HashMap<Int, Path>()
 
     override fun toString(): String {
         return "$name=$flowRate"
@@ -16,48 +14,10 @@ typealias Data = HashMap<String, Valve>
 typealias Solution = Int
 typealias Solution2 = Solution
 
-enum class ActionType { MOVE_TO, OPEN }
-data class Action(val actionType: ActionType, val step: Int, val valve: Valve) {
-    override fun toString(): String {
-        return "You ${actionType.toString().toLowerCase()} valve ${valve.name}. (step $step)"
-    }
-
-    val score = if (actionType == ActionType.OPEN) (step - 1) * valve.flowRate else 0
-}
-class Path(val actions: ArrayDeque<Action> = ArrayDeque(), val openedValves: HashSet<String> = HashSet()) {
-    val last: Action
-        get() = actions.last()
-
-    fun move(valve: Valve, step: Int) = pathWithAction(ActionType.MOVE_TO, valve, step)
-
-    fun open(valve: Valve, step: Int): Path? {
-        if (!canOpen(valve) || step <= 0) {
-            return null
-        }
-        return pathWithAction(ActionType.OPEN, valve, step).also { it.openedValves.add(valve.name) }
-    }
-
-    private fun pathWithAction(actionType: ActionType, valve: Valve, step: Int) =
-        Path(
-            ArrayDeque<Action>().also { it.add(Action(actionType, step, valve)) },
-            HashSet(openedValves)
-        )
-
-    fun canOpen(valve: Valve) = valve.flowRate > 0 && !openedValves.contains(valve.name)
-    fun add(path: Path): Path {
-        return Path(
-            ArrayDeque(actions + path.actions),
-            HashSet(openedValves + path.openedValves)
-        )
-    }
-
-    val score = actions.fold(0) { acc, action -> acc + action.score }
-}
-
 fun main() {
     try {
         val puz = Puzzle16()
-        val solution1 = puz.solvePuzzle("inputsTest.txt", Data())
+        val solution1 = puz.solvePuzzle("inputs.txt", Data())
         println("Solution1: $solution1")
 
         val solution2 = puz.solvePuzzle2("inputsTest.txt", Data())
@@ -67,6 +27,7 @@ fun main() {
     }
 }
 
+data class State(val time: Int, val valveName: String, val score: Int, val opened: Set<String> = HashSet())
 class Puzzle16 : Base<Data, Solution?, Solution2?>() {
     override fun parseLine(line: String, data: Data) {
         val valve = line.substringAfter("Valve ").substringBefore(" has")
@@ -76,67 +37,46 @@ class Puzzle16 : Base<Data, Solution?, Solution2?>() {
     }
 
     override fun computeSolution(data: Data): Solution {
-        val start: Valve = data["AA"]!!
-        
-        val path = Path().move(start, 0)
-        val solution = findBestPath(start, path, 30, data)
+        val rootState = State(1, "AA", 0)
+        val cache = HashMap<Pair<Int, String>, Int>()
 
-        val openValves = ArrayList<String>()
-        var totalFlowRate = 0
-        solution.actions.forEachIndexed { index, action ->
-            println("== Minute $index ==")
-            if (openValves.isEmpty()) {
-                println("No valves are open")
-            } else {
-                println("Valves $openValves are open, releasing $totalFlowRate pressure.")
-            }
-            if (action.actionType == ActionType.OPEN) {
-                openValves.add(action.valve.name)
-                totalFlowRate += action.valve.flowRate
-            }
-            println(action)
-            println()
-        }
-        return solution.score
-    }
+        val stack = ArrayDeque<State>()
+        stack.add(rootState)
 
-    private fun findBestPath(valve: Valve, thisPath: Path, numStepsLeft: Int, data: Data): Path {
-        val bestChild: Path? = when {
-            numStepsLeft in 0..1 -> null
-            numStepsLeft == 2 -> thisPath.open(valve, numStepsLeft) ?: thisPath.move(valve, numStepsLeft)
-            valve.children.isEmpty() -> null
-            else -> {
-                valve.bestFlowRate.getOrPut(numStepsLeft) {
-                    var currentBest: Path? = null
-                    val openPath = thisPath.open(valve, numStepsLeft)
-                    if (openPath != null) {
-                        currentBest = bestChildrenPath(openPath, valve.children, numStepsLeft - 1, data)
-                    }
-                    val bestChildrenPath = bestChildrenPath(thisPath, valve.children, numStepsLeft, data)
-                    val bestPath = bestPath(currentBest, bestChildrenPath)
-                    bestPath!!
-                }
+        var best = 0
+        while (stack.isNotEmpty()) {
+            val current = stack.removeFirst()
+            val time = current.time
+            val name = current.valveName
+            val score = current.score
+            val opened = current.opened
+
+            val cacheKey = Pair(time, name)
+            if (cache.getOrDefault(cacheKey, -1) >= score) {
+                // we cannot do any better, so skip it
+                continue
+            }
+            cache[cacheKey] = score
+
+            if (time == 30) {
+                best = max(best, score)
+                continue
+            }
+
+            val valve = data[name]!!
+            if (valve.flowRate > 0 && !opened.contains(name)) {
+                val newOpened = opened + name
+                val newScore = score + newOpened.sumOf { data[it]!!.flowRate }
+                stack.add(State(time + 1, name, newScore, newOpened))
+            }
+
+            val newScore = score + opened.sumOf { data[it]!!.flowRate }
+            valve.children.forEach { child ->
+                stack.add(State(time + 1, child, newScore, opened))
             }
         }
-
-        return if (bestChild != null) thisPath.add(bestChild) else thisPath
+        return best
     }
-
-    private fun bestChildrenPath(path: Path, children: List<String>, numStepsLeft: Int, data: Data): Path? {
-        return bestPathFromList(children.map { childName ->
-            val child = data[childName]!!
-            findBestPath(child, path.move(child, numStepsLeft), numStepsLeft - 1, data)
-        })
-    }
-
-    private fun bestPathFromList(list: List<Path>) = list.reduce(::bestPath)
-    private fun bestPath(path1: Path?, path2: Path?) =
-        when {
-            path1 == null -> path2
-            path2 == null -> path1
-            path1.score > path2.score -> path1
-            else -> path2
-        }
 
     override fun computeSolution2(data: Data): Solution2 {
         return 0
