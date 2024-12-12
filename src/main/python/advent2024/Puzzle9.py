8e78c5c1b6273d0cd332b2a470f121bbaae75f06
Erik Currin <ehc12345678@gmail.com>
2024-12-11 21:14:21 -0500

    Part 2 of 9, didn't fly but test data passes

--- src/main/python/advent2024/Puzzle9.py
+++ src/main/python/advent2024/Puzzle9.py
@@ -1,18 +1,23 @@
 from Base import Base
-from advent2024.DoublyLinkedList import DoublyLinkedList
+from advent2024.DoublyLinkedList import DoublyLinkedList, Node
 
 class NodeData:
-  def __init__(self, ordinal: int, free_space: bool):
+  def __init__(self, ordinal: int, free_space: bool, block_size: int = 1):
     self.ordinal = ordinal
     self.free_space = free_space
+    self.block_size = block_size
 
 MyDataType = DoublyLinkedList[NodeData]
 
 class Puzzle(Base[MyDataType, int, int]):
+    def __init__(self):
+      self.split_blocks = True
+
     def main(self):
       solution1: int = self.solve_puzzle("puzzle9/inputs.txt", MyDataType())  # type: ignore
       print("Solution1: {}".format(solution1))
 
+      self.split_blocks = False
       solution2: int = self.solve_puzzle2("puzzle9/inputs.txt", MyDataType()) # type: ignore
       print("Solution2: {}".format(solution2))
 
@@ -21,8 +26,14 @@ class Puzzle(Base[MyDataType, int, int]):
       for i in range(0, len(line)):
         block_size = int(line[i])
         free_space = (i % 2) != 0
-        for n in range(block_size):
-          data.append(NodeData(ordinal, free_space))
+        if self.split_blocks:
+          for n in range(block_size):
+            data.append(NodeData(ordinal, free_space))
+        else:
+          if free_space:
+            data.append(NodeData(0, free_space, block_size))
+          else:
+            data.append(NodeData(ordinal, free_space, block_size))
 
         if not free_space:
           ordinal += 1
@@ -39,10 +50,11 @@ class Puzzle(Base[MyDataType, int, int]):
 
     def print_list(self, data: MyDataType) -> None:
         for node in data:
-          if node.free_space:
-            print(".", end='')
-          else:
-            print(node.ordinal, end='')
+          for i in range(node.block_size):
+            if node.free_space:
+              print(".", end='')
+            else:
+              print(node.ordinal, end='')
         print()
 
     def rearrange_list(self, data: MyDataType) -> MyDataType:
@@ -79,8 +91,45 @@ class Puzzle(Base[MyDataType, int, int]):
 
       return data
 
+    # Too high: 6388294593135
     def compute_solution2(self, data: MyDataType):
-      pass
-    
+      data = self.rearrange_list_whole_blocks(data)
+      ret = 0
+
+      i = 0
+      for node in data:
+        for n in range(node.block_size):
+          ret += i * node.ordinal
+          i += 1
+      return ret
+
+    def rearrange_list_whole_blocks(self, data: MyDataType) -> MyDataType:
+      back = data.tail
+      while back is not None:
+        back_prev = back.prev
+        if not back.value.free_space:
+          first_free = self.find_free_space(data, back, back.value.free_space)
+          if first_free:
+            remaining_space = first_free.value.block_size - back.value.block_size
+            data.delete_node(back)
+            data.insert_after_node(back_prev, Node(NodeData(0, True, back.value.block_size)))
+            data.insert_after_node(first_free.prev, back)
+            if remaining_space > 0:
+              first_free.value.block_size = remaining_space
+            else:
+              data.delete_node(first_free)
+
+        back = back_prev
+      return data
+
+    def find_free_space(self, data: MyDataType, back: Node[NodeData], size: int) -> Node[NodeData] | None:
+      first_free_space = data.head
+      while first_free_space is not None and first_free_space != back:
+        remaining_space = first_free_space.value.block_size - back.value.block_size
+        if first_free_space.value.free_space and remaining_space >= 0:
+          return first_free_space
+        first_free_space = first_free_space.next
+      return None
+
 puzzle: Puzzle = Puzzle()
 puzzle.main()
