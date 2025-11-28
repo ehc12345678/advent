eeb3391f8e86762a3843a19838d770392e43bf68
Erik Currin <ehc12345678@gmail.com>
2025-11-28 15:25:19 -0500

    Add some Python puzzle

--- src/main/python/advent2024/BookReader.py
+++ src/main/python/advent2024/BookReader.py
@@ -1,21 +1,99 @@
 from Base import Base
-from typing import Tuple
+from collections.abc import Callable
+import re
 
-MyDataType = list[int]
+LABEL_MAP = {
+  "Ti": "title",
+  "Au": "author",
+  "Se": "series",
+  "Gn": "genre",
+  "Co": "collection",
+  "Nb": "notebook"
+}
+
+class BookNode:
+  def __init__(self, line):
+    self.children = []
+
+    raw_indent = self.find_first_non_space(line)
+    rest_line = line[raw_indent:]
+    regex = r"^(- )?(.+?)\: (.*)$"
+    m = re.match(regex, rest_line)
+
+    attributes = {}
+    if m is not None:
+      minus = m.group(1)
+      label = m.group(2)
+      value = m.group(3)
+      if minus is not None:
+        attributes["have"] = False
+        raw_indent = raw_indent + 2
+      if re.match(r"^\d*$", label):
+        attributes["ordinal"] = int(label)
+        label = "Ti"
+      if label.endswith("(hc)"):
+        attributes["hard_cover"] = True
+        label = label[:-4].strip()
+
+      attributes[LABEL_MAP.get(label, label)] = value
+    self.raw_indent_level = raw_indent
+    self.attributes = attributes
+
+  def merge_attributes(self, attributes):
+    self.attributes = attributes | self.attributes
+
+  def find_first_non_space(self, line):
+    for i, char in enumerate(line):
+      if not char.isspace():
+        return i
+    return 0
+
+  def __str__(self):
+    return self.attributes.__str__()
+
+MyDataType = list[BookNode]
 
 class Puzzle(Base[MyDataType, int, int]):
+    ReadLineFunc = Callable[..., MyDataType]
+
     def main(self):
-      solution1: int = self.solve_puzzle("puzzleXYZ/inputs.txt", MyDataType())  # type: ignore
+      solution1: int = self.solve_puzzle("books/inputs.txt", MyDataType())  # type: ignore
       print("Solution1: {}".format(solution1))
 
-      solution2: int = self.solve_puzzle2("puzzleXYZ/inputs.txt", MyDataType()) # type: ignore
+      solution2: int = self.solve_puzzle2("books/inputs.txt", MyDataType()) # type: ignore
       print("Solution2: {}".format(solution2))
 
+    def read_input(self, filename: str, data: MyDataType, read_func: ReadLineFunc) -> MyDataType:
+      with open(filename) as file:
+        for line in file:
+          read_func(line, data)  # type: ignore
+      return data
+
     def read_one_line(self, line: str, data: MyDataType):
-      data.append(int(line))
+      data.append(BookNode(line))
 
     def compute_solution(self, data: MyDataType) -> int:
-      pass
+      root = BookNode("root")
+      root.raw_indent_level = -1
+
+      all_books = []
+      node_stack = [root]
+
+      for i in range(len(data)):
+         node = data[i]
+
+         while node.raw_indent_level <= node_stack[-1].raw_indent_level:
+             node_stack.pop()
+
+         parent = node_stack[-1]
+         node.merge_attributes(parent.attributes)
+
+         if i + 1 == len(data) or node.raw_indent_level >= data[i + 1].raw_indent_level:
+             all_books.append(node)
+         else:
+             node_stack.append(node)
+
+      return len(all_books)
 
     def compute_solution2(self, data: MyDataType):
       pass
--- src/main/python/advent2024/Puzzle11.py
+++ src/main/python/advent2024/Puzzle11.py
@@ -2,23 +2,55 @@ from Base import Base
 from typing import Tuple
 
 MyDataType = list[int]
+NumAndSteps = Tuple[int, int]
 
 class Puzzle(Base[MyDataType, int, int]):
     def main(self):
-      solution1: int = self.solve_puzzle("puzzleXYZ/inputs.txt", [])  # type: ignore
+      solution1: int = self.solve_puzzle("puzzle11/inputsTest.txt", MyDataType())  # type: ignore
       print("Solution1: {}".format(solution1))
 
-      solution2: int = self.solve_puzzle2("puzzleXYZ/inputs.txt", []) # type: ignore
+      solution2: int = self.solve_puzzle2("puzzle11/inputsTest.txt", MyDataType()) # type: ignore
       print("Solution2: {}".format(solution2))
 
     def read_one_line(self, line: str, data: MyDataType):
-      data.append(int(line))
+      data += map(int, line.split(" "))
 
     def compute_solution(self, data: MyDataType) -> int:
-      pass
+      return self.solve_for_n_steps(data, 25)
 
     def compute_solution2(self, data: MyDataType):
-      pass
-    
+      return self.solve_for_n_steps(data, 75)
+
+    def solve_for_n_steps(self, data: MyDataType, steps: int) -> int:
+      calculated = dict[NumAndSteps, int]()
+      todo = dict[NumAndSteps, list[int]]()
+
+      to_solve = map(lambda x: (x, steps), data)
+      while self.unsolved(to_solve):
+        print("{}: {}", len(data), data)
+        data = self.do_one_step(data)
+      return len(data)
+
+    def do_one_step(self, data: MyDataType) -> MyDataType:
+      new_data = MyDataType()
+      for entry in data:
+        self.next_value(entry, new_data)
+      return new_data
+
+    def next_value(self, entry, new_data):
+      str_entry = str(entry)
+      if entry == 0:
+        new_data.append(1)
+      elif (len(str_entry) % 2) == 0:
+        first_half = int(str_entry[0:len(str_entry) // 2])
+        second_half = int(str_entry[len(str_entry) // 2:])
+        new_data.append(int(first_half))
+        new_data.append(int(second_half))
+      else:
+        new_data.append(2024 * entry)
+
+    def unsolved(self, to_solve) -> bool:
+      return not all(isinstance(ele, int) for ele in to_solve)
+
 puzzle: Puzzle = Puzzle()
 puzzle.main()
